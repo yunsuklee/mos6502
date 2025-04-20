0769450b69dbf066aa91b936d9ac15ed32f27884
Sergio Yunsuk Lee <sergioleesp@gmail.com>
2025-04-20 17:41:01 +0200

    fix: Adjust names after status refactor

--- src/core/instructions.c
+++ src/core/instructions.c
@@ -2,64 +2,64 @@
 
 void adc(cpu_t *cpu, instruction_t *instruction) {
     uint8_t a = cpu->accumulator;
-    uint16_t aux = (uint16_t)cpu->accumulator + (uint16_t)(*instruction->operand) + (uint16_t)get_status(&cpu->status_register, CARRY);
+    uint16_t aux = (uint16_t)cpu->accumulator + (uint16_t)(*instruction->operand) + (uint16_t)status_get_flag(&cpu->status_register, FLAG_CARRY);
     cpu->accumulator = aux;
 
-    set_carry(&cpu->status_register,aux);
-    set_zero(&cpu->status_register, cpu->accumulator);
-    set_negative(&cpu->status_register, cpu->accumulator);
-    set_overflow(&cpu->status_register, a, *instruction->operand, cpu->accumulator);
+    status_set_carry_flag(&cpu->status_register,aux);
+    status_set_zero_flag(&cpu->status_register, cpu->accumulator);
+    status_set_negative_flag(&cpu->status_register, cpu->accumulator);
+    status_set_overflow_flag(&cpu->status_register, a, *instruction->operand, cpu->accumulator);
 }
 
 void and(cpu_t *cpu, instruction_t *instruction) {
     cpu->accumulator &= *instruction->operand;
 
-    set_zero(&cpu->status_register, cpu->accumulator);
-    set_negative(&cpu->status_register, cpu->accumulator);
+    status_set_zero_flag(&cpu->status_register, cpu->accumulator);
+    status_set_negative_flag(&cpu->status_register, cpu->accumulator);
 }
 
 void asl(cpu_t *cpu, instruction_t *instruction) {
     uint16_t aux = *instruction->operand << 1;
     *instruction->operand = aux;
 
-    set_zero(&cpu->status_register, *instruction->operand);
-    set_negative(&cpu->status_register, *instruction->operand);
-    set_carry(&cpu->status_register, aux);
+    status_set_zero_flag(&cpu->status_register, *instruction->operand);
+    status_set_negative_flag(&cpu->status_register, *instruction->operand);
+    status_set_carry_flag(&cpu->status_register, aux);
 }
 
 void bcc(cpu_t *cpu, instruction_t *instruction) {
-    if(!get_status(&cpu->status_register, CARRY))
+    if(!status_get_flag(&cpu->status_register, FLAG_CARRY))
         cpu->program_counter += *((int8_t *)instruction->operand);
 }
 
 void bcs(cpu_t *cpu, instruction_t *instruction) {
-    if(get_status(&cpu->status_register, CARRY))
+    if(status_get_flag(&cpu->status_register, FLAG_CARRY))
         cpu->program_counter += *((int8_t *)instruction->operand);
 }
 
 void beq(cpu_t *cpu, instruction_t *instruction) {
-    if(get_status(&cpu->status_register, ZERO))
+    if(status_get_flag(&cpu->status_register, FLAG_ZERO))
         cpu->program_counter += *((int8_t *)instruction->operand);
 }
 
 void bit(cpu_t *cpu, instruction_t *instruction) {
-    set_negative(&cpu->status_register, *instruction->operand);
-    set_zero(&cpu->status_register, cpu->accumulator & (*instruction->operand));
-    set_status(&cpu->status_register, OVERFLOW, (*instruction->operand) & 0x40);
+    status_set_negative_flag(&cpu->status_register, *instruction->operand);
+    status_set_zero_flag(&cpu->status_register, cpu->accumulator & (*instruction->operand));
+    status_set_flag(&cpu->status_register, FLAG_OVERFLOW, (*instruction->operand) & 0x40);
 }
 
 void bmi(cpu_t *cpu, instruction_t *instruction) {
-    if(get_status(&cpu->status_register, NEGATIVE))
+    if(status_get_flag(&cpu->status_register, FLAG_NEGATIVE))
         cpu->program_counter += *((int8_t *)instruction->operand);
 }
 
 void bne(cpu_t *cpu, instruction_t *instruction) {
-    if(!get_status(&cpu->status_register, ZERO))
+    if(!status_get_flag(&cpu->status_register, FLAG_ZERO))
         cpu->program_counter += *((int8_t *)instruction->operand);
 }
 
 void bpl(cpu_t *cpu, instruction_t *instruction) {
-    if(!get_status(&cpu->status_register, NEGATIVE))
+    if(!status_get_flag(&cpu->status_register, FLAG_NEGATIVE))
         cpu->program_counter += *((int8_t *)instruction->operand);
 }
 
@@ -72,46 +72,46 @@ void brk(cpu_t *cpu, instruction_t *instruction) {
     cpu->memory[0x100 | cpu->stack_pointer] = cpu->status_register | (1 << 4) | (1 << 5);
     cpu->stack_pointer--;
 
-    set_status(&cpu->status_register, INTERRUPT_DISABLE, 1);
+    status_set_flag(&cpu->status_register, FLAG_INTERRUPT_DISABLE, 1);
 
     cpu->program_counter = cpu->memory[0xFFFF] << 8;
     cpu->program_counter |= cpu->memory[0xFFFE];
 }
 
 void bvc(cpu_t *cpu, instruction_t *instruction) {
-    if(!get_status(&cpu->status_register , OVERFLOW))
+    if(!status_get_flag(&cpu->status_register , FLAG_OVERFLOW))
         cpu->program_counter += *((int8_t *)instruction->operand);
 }
 
 void bvs(cpu_t *cpu, instruction_t *instruction) {
-    if(get_status(&cpu->status_register, OVERFLOW))
+    if(status_get_flag(&cpu->status_register, FLAG_OVERFLOW))
         cpu->program_counter += *((int8_t *)instruction->operand);
 }
 
-void clear(cpu_t *cpu, flag_t flag) {
-    set_status(&cpu->status_register, flag, 0);
+void clear(cpu_t *cpu, status_flag_t flag) {
+    status_set_flag(&cpu->status_register, flag, 0);
 }
 
 void clc(cpu_t *cpu, instruction_t *instruction) {
-    clear(cpu, CARRY);
+    clear(cpu, FLAG_CARRY);
 }
 
 void cld(cpu_t *cpu, instruction_t *instruction) {
-    clear(cpu, DECIMAL);
+    clear(cpu, FLAG_DECIMAL);
 }
 
 void cli(cpu_t *cpu, instruction_t *instruction) {
-    clear(cpu, INTERRUPT_DISABLE);
+    clear(cpu, FLAG_INTERRUPT_DISABLE);
 }
 
 void clv(cpu_t *cpu, instruction_t *instruction) {
-     clear(cpu, OVERFLOW);
+     clear(cpu, FLAG_OVERFLOW);
 }
 
 void compare(cpu_t *cpu, instruction_t *instruction, uint8_t *registro) {
-    set_status(&cpu->status_register, ZERO, *registro == (*instruction->operand));
-    set_status(&cpu->status_register, CARRY, *registro >= (*instruction->operand));
-    set_status(&cpu->status_register, NEGATIVE, *registro < (*instruction->operand));
+    status_set_flag(&cpu->status_register, FLAG_ZERO, *registro == (*instruction->operand));
+    status_set_flag(&cpu->status_register, FLAG_CARRY, *registro >= (*instruction->operand));
+    status_set_flag(&cpu->status_register, FLAG_NEGATIVE, *registro < (*instruction->operand));
 }
 
 void cmp(cpu_t *cpu, instruction_t *instruction) {
@@ -129,8 +129,8 @@ void cpy(cpu_t *cpu, instruction_t *instruction) {
 void decrement(cpu_t *cpu, uint8_t *registro) {
     *registro -= 1;
 
-    set_zero(&cpu->status_register, *registro);
-    set_negative(&cpu->status_register, *registro);
+    status_set_zero_flag(&cpu->status_register, *registro);
+    status_set_negative_flag(&cpu->status_register, *registro);
  }
 
 void dec(cpu_t *cpu, instruction_t *instruction) {
@@ -148,15 +148,15 @@ void dey(cpu_t *cpu, instruction_t *instruction) {
 void eor(cpu_t *cpu, instruction_t *instruction) {
     cpu->accumulator ^= *instruction->operand;
 
-    set_zero(&cpu->status_register, cpu->accumulator);
-    set_negative(&cpu->status_register, cpu->accumulator);
+    status_set_zero_flag(&cpu->status_register, cpu->accumulator);
+    status_set_negative_flag(&cpu->status_register, cpu->accumulator);
 }
 
 void increment(cpu_t *cpu, uint8_t *registro) {
     *registro += 1;
 
-    set_zero(&cpu->status_register, *registro);
-    set_negative(&cpu->status_register, *registro);
+    status_set_zero_flag(&cpu->status_register, *registro);
+    status_set_negative_flag(&cpu->status_register, *registro);
 }
 
 void inc(cpu_t *cpu, instruction_t *instruction) {
@@ -187,8 +187,8 @@ void jsr(cpu_t *cpu,instruction_t *instruction) {
 void load(cpu_t *cpu, instruction_t *instruction, uint8_t *registro) {
     *registro = *instruction->operand;
 
-    set_zero(&cpu->status_register, *instruction->operand);
-    set_negative(&cpu->status_register, *instruction->operand);
+    status_set_zero_flag(&cpu->status_register, *instruction->operand);
+    status_set_negative_flag(&cpu->status_register, *instruction->operand);
 }
 
 void lda(cpu_t *cpu, instruction_t *instruction) {
@@ -207,9 +207,9 @@ void lsr(cpu_t *cpu,instruction_t *instruction) {
     uint8_t bit_caido = (*instruction->operand & 0x01);
     *instruction->operand >>= 1;
 
-    set_zero(&cpu->status_register, *instruction->operand);
-    set_negative(&cpu->status_register, *instruction->operand);
-    set_status(&cpu->status_register, CARRY, bit_caido);
+    status_set_zero_flag(&cpu->status_register, *instruction->operand);
+    status_set_negative_flag(&cpu->status_register, *instruction->operand);
+    status_set_flag(&cpu->status_register, FLAG_CARRY, bit_caido);
 }
 
 void nop(cpu_t *cpu, instruction_t *instruction) {}
@@ -217,8 +217,8 @@ void nop(cpu_t *cpu, instruction_t *instruction) {}
 void ora(cpu_t *cpu, instruction_t *instruction) {
     cpu->accumulator |= *instruction->operand;
 
-    set_zero(&cpu->status_register, cpu->accumulator);
-    set_negative(&cpu->status_register, cpu->accumulator);
+    status_set_zero_flag(&cpu->status_register, cpu->accumulator);
+    status_set_negative_flag(&cpu->status_register, cpu->accumulator);
 }
 
 void pha(cpu_t *cpu, instruction_t *instruction) {
@@ -235,8 +235,8 @@ void pla(cpu_t *cpu, instruction_t *instruction) {
     cpu->stack_pointer++;
     cpu->accumulator = cpu->memory[0x0100 | cpu->stack_pointer];
 
-    set_zero(&cpu->status_register, cpu->accumulator);
-    set_negative(&cpu->status_register, cpu->accumulator);
+    status_set_zero_flag(&cpu->status_register, cpu->accumulator);
+    status_set_negative_flag(&cpu->status_register, cpu->accumulator);
 }
 
 void plp(cpu_t *cpu, instruction_t *instruction) {
@@ -245,17 +245,17 @@ void plp(cpu_t *cpu, instruction_t *instruction) {
 }
 
 void rol(cpu_t *cpu, instruction_t *instruction) {
-    rotate_left(&cpu->status_register, instruction->operand);
+    status_rotate_left(&cpu->status_register, instruction->operand);
 
-    set_zero(&cpu->status_register, *instruction->operand);
-    set_negative(&cpu->status_register, *instruction->operand);
+    status_set_zero_flag(&cpu->status_register, *instruction->operand);
+    status_set_negative_flag(&cpu->status_register, *instruction->operand);
 }
 
 void ror(cpu_t *cpu, instruction_t *instruction) {
-    rotate_right(&cpu->status_register, instruction->operand);
+    status_rotate_right(&cpu->status_register, instruction->operand);
 
-    set_zero(&cpu->status_register, *instruction->operand);
-    set_negative(&cpu->status_register, *instruction->operand);
+    status_set_zero_flag(&cpu->status_register, *instruction->operand);
+    status_set_negative_flag(&cpu->status_register, *instruction->operand);
 }
 
 void rti(cpu_t *cpu, instruction_t *instruction) {
@@ -278,25 +278,25 @@ void rts(cpu_t *cpu, instruction_t *instruction) {
 void sbc(cpu_t *cpu, instruction_t *instruction) {
     uint8_t a = cpu->accumulator;
     uint16_t m = (uint16_t)(*instruction->operand) ^ 0x00FF;
-    uint16_t aux = (uint16_t)cpu->accumulator + m + (uint16_t)get_status(&cpu->status_register, CARRY);
+    uint16_t aux = (uint16_t)cpu->accumulator + m + (uint16_t)status_get_flag(&cpu->status_register, FLAG_CARRY);
     cpu->accumulator = aux;
 
-    set_zero(&cpu->status_register, cpu->accumulator);
-    set_carry(&cpu->status_register, aux);
-    set_negative(&cpu->status_register, cpu->accumulator);
-    set_overflow(&cpu->status_register, a, m, cpu->accumulator);
+    status_set_zero_flag(&cpu->status_register, cpu->accumulator);
+    status_set_carry_flag(&cpu->status_register, aux);
+    status_set_negative_flag(&cpu->status_register, cpu->accumulator);
+    status_set_overflow_flag(&cpu->status_register, a, m, cpu->accumulator);
 }
 
 void sec(cpu_t *cpu, instruction_t *instruction) {
-    set_status(&cpu->status_register, CARRY, 1);
+    status_set_flag(&cpu->status_register, FLAG_CARRY, 1);
 }
 
 void sed(cpu_t *cpu,instruction_t *instruction) {
-    set_status(&cpu->status_register, DECIMAL, 1);
+    status_set_flag(&cpu->status_register, FLAG_DECIMAL, 1);
 }
 
 void sei(cpu_t *cpu,instruction_t *instruction) {
-    set_status(&cpu->status_register, INTERRUPT_DISABLE, 1);
+    status_set_flag(&cpu->status_register, FLAG_INTERRUPT_DISABLE, 1);
 }
 
 void sta(cpu_t *cpu, instruction_t *instruction) {
@@ -314,8 +314,8 @@ void sty(cpu_t *cpu, instruction_t *instruction) {
 void transfer(cpu_t *cpu, uint8_t *registro) {
     *registro = cpu->accumulator;
 
-    set_zero(&cpu->status_register, *registro);
-    set_negative(&cpu->status_register, *registro);
+    status_set_zero_flag(&cpu->status_register, *registro);
+    status_set_negative_flag(&cpu->status_register, *registro);
 }
 
 void tax(cpu_t *cpu,instruction_t *instruction) {
@@ -329,15 +329,15 @@ void tay(cpu_t *cpu, instruction_t *instruction) {
 void tsx(cpu_t *cpu,instruction_t *instruction) {
     cpu->x_index_register = cpu->stack_pointer;
 
-    set_zero(&cpu->status_register, cpu->x_index_register);
-    set_negative(&cpu->status_register, cpu->x_index_register);
+    status_set_zero_flag(&cpu->status_register, cpu->x_index_register);
+    status_set_negative_flag(&cpu->status_register, cpu->x_index_register);
 }
 
 void txa(cpu_t *cpu,instruction_t *instruction) {
     cpu->accumulator = cpu->x_index_register;
 
-    set_zero(&cpu->status_register, cpu->accumulator);
-    set_negative(&cpu->status_register, cpu->accumulator);
+    status_set_zero_flag(&cpu->status_register, cpu->accumulator);
+    status_set_negative_flag(&cpu->status_register, cpu->accumulator);
 }
 
 void txs(cpu_t *cpu,instruction_t *instruction) {
@@ -347,6 +347,6 @@ void txs(cpu_t *cpu,instruction_t *instruction) {
 void tya(cpu_t *cpu, instruction_t *instruction) {
     cpu->accumulator = cpu->y_index_register;
 
-    set_zero(&cpu->status_register, cpu->accumulator);
-    set_negative(&cpu->status_register, cpu->accumulator);
+    status_set_zero_flag(&cpu->status_register, cpu->accumulator);
+    status_set_negative_flag(&cpu->status_register, cpu->accumulator);
 }
