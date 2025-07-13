983005d02454aeef7381960df576b58f427494b8
Sergio Yunsuk Lee <sergioleesp@gmail.com>
2025-07-13 19:20:53 +0200

    refactor: Improved argument handling

--- src/main.c
+++ src/main.c
@@ -4,14 +4,27 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-#include "core/opcodes.h"
 #include "core/cpu.h"
 
-// Inicializa los registros del microprocesador
+#define DEFAULT_LOG_PATH "log/log.txt"
+
+void print_usage(const char *program_name) {
+    printf("Usage: %s <rom_file> [-halt <address> | -cycles <count>] [-log <log_file>]\n\n", program_name);
+    printf("Arguments:\n");
+    printf("  <rom_file>           ROM file to load (required)\n");
+    printf("  -halt <address>      Stop execution at hexadecimal address (e.g., 336d)\n");
+    printf("  -cycles <count>      Stop execution after specified number of cycles\n");
+    printf("  -log <log_file>      Log CPU state to file (optional, defaults to %s)\n\n", DEFAULT_LOG_PATH);
+    printf("Examples:\n");
+    printf("  %s game.rom -cycles 1000\n", program_name);
+    printf("  %s game.rom -halt 336d -log debug.txt\n", program_name);
+    printf("  %s game.rom -cycles 5000 -log states.log\n", program_name);
+}
+
 int main(int argc, char *argv[]) {
-    // Si no se pasan los parametros necesarios
-    if(argc < 6) {
-        printf("El programa debe ejecutarse con los siguientes parametros:\n./mos6502 [nombre de archivo rom]\n-log [nombre de archivo para setear el log]\n-halt o -ciclos [numero de instruccion hexadecimal o numero de ciclos]\n");
+    // Minimum arguments: program_name, rom_file, halt_or_cycles_flag, value
+    if (argc < 4) {
+        print_usage(argv[0]);
         return 1;
     }
 
@@ -24,7 +37,6 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    // Condicion de corte con parametro de ciclos (83007452)
     if(!strcmp(argv[4], "-ciclos")) {
         long ejecutados;
         ejecutados = atoi(argv[5]);
@@ -36,7 +48,6 @@ int main(int argc, char *argv[]) {
             cpu_execute_instruction(cpu);
         }
     }
-    // Condicion de corte con parametro de instruccion (336d)
     else if(!strcmp(argv[4], "-halt")) {
         uint16_t aux;
         aux = (uint16_t)strtol(argv[5], NULL, 16);
@@ -52,8 +63,9 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    printf("Ciclos finales: %ld\n", cpu->total_execution_cycles);
+    printf("Total execution cycles: %ld\n", cpu->total_execution_cycles);
     cpu_destroy(cpu);
     printf("Success!\n");
     return 0;
 }
+
